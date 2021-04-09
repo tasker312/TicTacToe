19db79fbadb8893ab3241262210cb72c7c24e70f
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-04-09 07:59:23 +0300

    Add hard bot

--- src/Bot.java
+++ src/Bot.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.awt.Point;
 
 public class Bot {
     public static final char botMove = TicTacToe.player2;
@@ -19,6 +19,9 @@ public class Bot {
                 break;
             case MEDIUM:
                 move = getMoveMedium();
+                break;
+            case HARD:
+                move = getMoveHard();
                 break;
         }
         return move;
@@ -63,4 +66,82 @@ public class Bot {
         //get random move
         return getMoveEasy();
     }
+
+    private Point getMoveHard() {
+        int score = Integer.MAX_VALUE;
+        Point move = null;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == TicTacToe.NULL_CHAR) {
+                    board[i][j] = botMove;
+                    int ijScore = miniMax(true);
+                    if (ijScore < score) {
+                        score = ijScore;
+                        move = new Point(i, j);
+                    }
+                    board[i][j] = TicTacToe.NULL_CHAR;
+                }
+            }
+        }
+        return move;
+    }
+
+    private int miniMax(boolean isMaximizing) {
+        if (TicTacToe.checkWinner(board) > 0) {
+            return getScoreByWinner(TicTacToe.checkWinner(board));
+        }
+        int score;
+        if (isMaximizing) {
+            //best player move
+            score = Integer.MIN_VALUE;
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j] == TicTacToe.NULL_CHAR) {
+                        board[i][j] = playerMove;
+                        int ijScore = miniMax(false);
+                        if (ijScore > score) {
+                            score = ijScore;
+                        }
+                        board[i][j] = TicTacToe.NULL_CHAR;
+                    }
+                }
+            }
+        } else {
+            //best bot move
+            score = Integer.MAX_VALUE;
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (board[i][j] == TicTacToe.NULL_CHAR) {
+                        board[i][j] = botMove;
+                        int ijScore = miniMax(true);
+                        if (ijScore < score) {
+                            score = ijScore;
+                        }
+                        board[i][j] = TicTacToe.NULL_CHAR;
+                    }
+                }
+            }
+        }
+        return score;
+    }
+
+    /*
+    X-win - +1
+    O-win - -1
+    Draw  -  0
+     */
+    private int getScoreByWinner(int winner) {
+        int score = 0;
+        switch (winner) {
+            case (int) 'X':
+                score++;
+                break;
+            case (int) 'O':
+                score--;
+                break;
+        }
+        return score;
+    }
+
+
 }
